7ecfdd0e2772113a772a42603f876463c5cced44
benlipkin <bglipkin@gmail.com>
2023-03-06 16:25:33 -0500

    clear train log before starting

--- conwin/__init__.py
+++ conwin/__init__.py
@@ -1,11 +1,15 @@
 import logging
+import pathlib
 import sys
 
+fname = pathlib.Path(__file__).parents[1] / "train.log"
+if fname.exists():
+    fname.unlink()
 
 logging.basicConfig(
     level=logging.INFO,
     handlers=[
-        logging.FileHandler(filename="train.log"),
+        logging.FileHandler(filename=fname),
         logging.StreamHandler(stream=sys.stdout),
     ],
 )
